0f17ee64758fd3eae3bde6368f208e84925927b6
frank <itouch@188.com>
2015-11-17 22:07:03 +0800

    create
    Signed-off-by: frank <itouch@188.com>

---
+++ SVGParser/SVGParser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SVGParser
+//
+//  Created by WG on 15/11/17.
+//  Copyright © 2015年 WG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SVGParser/SVGParser/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SVGParser
+//
+//  Created by WG on 15/11/17.
+//  Copyright © 2015年 WG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
